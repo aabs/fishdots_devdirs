bbfba273c670429ce77c69cdd7671cafd29b66f6
Andrew Matthews <matthews.andrew@gmail.com>
2020-04-11 23:05:48 +1000

    more tinkering

--- init/rc0.d/01-env.fish
+++ init/rc0.d/01-env.fish
@@ -1,13 +1,1 @@
-if test -e /mnt/d/
-    set -x SYNC_HOME /mnt/d/Synchronised
-else if test -e /d/
-    set -x SYNC_HOME /d/Synchronised
-else if test -e /media/aabs/DATA/
-    set -x SYNC_HOME /media/aabs/DATA/Synchronised
-else if test -e /cygdrive/d/
-    set -x SYNC_HOME /cygdrive/d/Synchronised
-else
-    set_color red
-    echo "[ERROR] unable to find the Sync directory"
-    set_color normal
-end
+
